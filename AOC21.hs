3ec84d5b9e1e1fbbd3b69d106145531f46344f53
Moritz Grauel <mgrauel@atlassian.com>
2020-12-21 17:10:32 +1100

    Cleanup

--- AOC21.hs
+++ AOC21.hs
@@ -1,12 +1,13 @@
 module AOC21 where
 
-import Text.Parsec.ByteString (Parser, parseFromFile)
-import Text.Parsec ( many1, letter, sepBy1, space, string, endOfLine, try, endBy1, char )
-import qualified Data.Set as S
-import qualified Data.Map.Strict as M
-import Data.Either (fromRight)
-import Data.List (nub, sortBy, intercalate)
-import Data.Functor ((<&>))
+import           Data.Either            (fromRight)
+import           Data.Functor           ((<&>))
+import           Data.List              (intercalate, nub, sortBy)
+import qualified Data.Map.Strict        as M
+import qualified Data.Set               as S
+import           Text.Parsec            (endBy1, endOfLine, letter, many1,
+                                         sepBy1, space, string, try)
+import           Text.Parsec.ByteString (Parser, parseFromFile)
 
 foodParser :: Parser Food
 foodParser = do
@@ -32,7 +33,7 @@ toCandidateSet ((ingredients, allergen):fs) c = toCandidateSet fs (foldl f c all
         f c' a = M.alter g a c'
         g :: Maybe (S.Set Ingredient) -> Maybe (S.Set Ingredient)
         g (Just s) = Just $ S.intersection s $ S.fromList ingredients
-        g Nothing = Just $ S.fromList ingredients
+        g Nothing  = Just $ S.fromList ingredients
 
 -- 1977
 solution1 :: IO Int
@@ -41,33 +42,24 @@ solution1 = do
   let ingredients = nub . concat $ fst <$> food
   let candidates = toCandidateSet food M.empty
   let safeIngredients = S.fromList ingredients S.\\ M.foldr S.union S.empty candidates
-  return . sum $ S.toList safeIngredients <&> (\i -> length . filter (== i) $ (concat $ fst <$> food) )
+  return . sum $ S.toList safeIngredients <&> (\i -> length . filter (== i) $ concat (fst <$> food) )
 
 sanitize :: Candidates -> Candidates
 sanitize c = M.fromList $ go <$> M.assocs c
-  where go :: (Allergen, (S.Set Ingredient)) -> (Allergen, (S.Set Ingredient))
+  where go :: (Allergen, S.Set Ingredient) -> (Allergen, S.Set Ingredient)
         go (a, s) | length s == 1 = (a, s)
-        go (a, s) = (a, M.foldl ((\s s' -> if length s' ==  1 then s S.\\ s' else s)) s c)
-        
-removeSafe :: Candidates -> S.Set Ingredient -> Candidates
-removeSafe c safe = M.fromList $ go <$> M.assocs c
-       where go :: (Allergen, (S.Set Ingredient)) -> (Allergen, (S.Set Ingredient))
-             go (a, s) | length s == 1 = (a, s)
-             go (a, s) = (a, s S.\\ safe)
+        go (a, orig_s) = (a, M.foldl (\s s' -> if length s' ==  1 then s S.\\ s' else s) orig_s c)
 
 untilStable :: Eq a => (a -> a) -> a -> a
 untilStable f a = let a' = f a in if a == a' then a else untilStable f a'
 
 pprint :: Candidates -> String
-pprint cs = intercalate "," $ (head . S.toList . snd) <$> (sortBy (\(i, _a) (i', _a') -> compare i i') $ M.toList cs)
+pprint cs = intercalate "," $ head . S.toList . snd <$> sortBy (\ (i, _a) (i', _a') -> compare i i') (M.toList cs)
 
 -- dpkvsdk,xmmpt,cxjqxbt,drbq,zmzq,mnrjrf,kjgl,rkcpxs
 solution2 :: IO String
 solution2 = do
   food <- fromRight [] <$> parseFromFile inputParser  "AOC21.input"
-  let ingredients = nub . concat $ fst <$> food
   let candidates = toCandidateSet food M.empty
-  let safeIngredients = S.fromList ingredients S.\\ M.foldr S.union S.empty candidates
-  let allergenes = untilStable sanitize (removeSafe candidates safeIngredients)
-  print $ allergenes
+  let allergenes = untilStable sanitize candidates
   return $ pprint allergenes
