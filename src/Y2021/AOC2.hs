8e51b593c638a5720fced38322704926fb822851
Moritz Grauel <mgrauel@atlassian.com>
2021-12-02 16:35:07 +1100

    Day 2

--- src/Y2021/AOC2.hs
+++ src/Y2021/AOC2.hs
@@ -1,25 +1,66 @@
 module Y2021.AOC2 where
 
+import Debug.Trace
 import Test.HUnit (Test (TestCase, TestList), assertEqual)
+import Text.Parsec (many1, newline, string, (<|>))
 import Text.Parsec.ByteString (Parser)
-import Util (Input, parseOrDie, (|>))
+import Util (Input, number, parseOrDie, (|>))
 
-inputParser :: Parser String
-inputParser = undefined
+data Direction = Up Int | Down Int | Forward Int deriving (Show, Eq)
 
+data Position = Position
+  { depth :: Int,
+    horizontal :: Int,
+    aim :: Int
+  }
+  deriving (Show, Eq)
+
+startingPos :: Position
+startingPos = Position 0 0 0
+
+directionParser :: Parser Direction
+directionParser = do
+  dir <- string "forward " *> pure Forward <|> string "down " *> pure Down <|> string "up " *> pure Up
+  dist <- number <* newline
+  pure $ dir dist
+
+inputParser :: Parser [Direction]
+inputParser = many1 directionParser
+
+result :: Position -> Int
+result (Position d h a) = d * h
+
+eval :: Position -> [Direction] -> Position
+eval = foldl step
+  where
+    step (Position d h a) (Up n) = Position (d - n) h a
+    step (Position d h a) (Down n) = Position (d + n) h a
+    step (Position d h a) (Forward n) = Position d (h + n) a
+
+eval' :: Position -> [Direction] -> Position
+eval' = foldl step
+  where
+    step (Position d h a) (Up n) = Position d h (a - n)
+    step (Position d h a) (Down n) = Position d h (a + n)
+    step (Position d h a) (Forward n) = Position (d + (a * n)) (h + n) a
+
+-- 1815044
 solution1 :: Input -> Int
 solution1 input =
   parseOrDie inputParser input
-    |> error "not yet"
+    |> eval startingPos
+    |> result
 
+-- 1739283308
 solution2 :: Input -> Int
 solution2 input =
   parseOrDie inputParser input
-    |> error "not yet"
+    |> eval' startingPos
+    |> result
 
 verify :: IO Input -> Test
 verify input =
   TestList
-    [ TestCase $ assertEqual "solution 1" undefined . solution1 =<< input,
-      TestCase $ assertEqual "solution 2" undefined . solution2 =<< input
+    [ TestCase $ assertEqual "solution 1" 1815044 . solution1 =<< input,
+      TestCase $ assertEqual "solution 2" 1739283308 . solution2 =<< input
     ]
