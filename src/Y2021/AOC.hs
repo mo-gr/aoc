c7085b0ffc942ffb5e533e7af7390522ed710a24
Moritz Grauel <mgrauel@atlassian.com>
2021-12-04 14:43:49 +1100

    d4 prep

--- src/Y2021/AOC.hs
+++ src/Y2021/AOC.hs
@@ -8,6 +8,7 @@ import qualified Util (withPath)
 import qualified Y2021.AOC1
 import qualified Y2021.AOC2
 import qualified Y2021.AOC3
+import qualified Y2021.AOC4
 
 year :: Year
 year = mkYear "2021" solution verify
@@ -16,6 +17,7 @@ solution :: Day -> (IO String, IO String)
 solution D1 = run Y2021.AOC1.solution1 Y2021.AOC1.solution2 $ loadInput "AOC1"
 solution D2 = run Y2021.AOC2.solution1 Y2021.AOC2.solution2 $ loadInput "AOC2"
 solution D3 = run Y2021.AOC3.solution1 Y2021.AOC3.solution2 $ loadInput "AOC3"
+solution D4 = run Y2021.AOC4.solution1 Y2021.AOC4.solution2 $ loadInput "AOC4"
 solution _ = error "not yet"
 
 loadInput :: String -> IO Input
@@ -29,7 +31,8 @@ verify =
   TestList
     [ TestLabel "Day 1" $ Y2021.AOC1.verify (loadInput "AOC1"),
       TestLabel "Day 2" $ Y2021.AOC2.verify (loadInput "AOC2"),
-      TestLabel "Day 3" $ Y2021.AOC3.verify (loadInput "AOC3")
+      TestLabel "Day 3" $ Y2021.AOC3.verify (loadInput "AOC3"),
+      TestLabel "Day 4" $ Y2021.AOC4.verify (loadInput "AOC4")
     ]
 
 inRepl1 :: Day -> IO String
