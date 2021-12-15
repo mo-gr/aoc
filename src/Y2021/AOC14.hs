a1ba8609bb35f1654fab5a97d72e2e1d4b197a51
Moritz Grauel <mgrauel@atlassian.com>
2021-12-15 14:50:34 +1100

    more effective approach

--- src/Y2021/AOC14.hs
+++ src/Y2021/AOC14.hs
@@ -6,10 +6,15 @@ import Test.HUnit (Test (TestCase, TestList), assertEqual)
 import Text.Parsec.ByteString (Parser)
 import Util (Input, parseOrDie, (|>), times)
 import Text.Parsec (many1, upper, newline, count, string)
-import Data.List (find, group, sort, minimum, maximum)
+import Data.List (find, group, sort, minimum, maximum, sortOn)
+import qualified Data.Map.Strict as M
+import Debug.Trace
 
 type Polymer = String
 type Reaction = (String, Char)
+type Reactions = M.Map String Char
+
+type PolymerTracker = M.Map String Int
 
 polymerParser :: Parser Polymer
 polymerParser = many1 upper <* newline
@@ -27,31 +32,60 @@ inputParser = do
   rs <- many1 reactionParser
   pure (p, rs)
 
-react :: Polymer -> [Reaction] -> Polymer
+dissect :: PolymerTracker -> Polymer -> PolymerTracker
+dissect pt [] = pt
+dissect pt (p:[]) = M.insertWith (+) [p] 1 pt
+dissect pt (p:p':ps) = M.insertWith (+) [p,p'] 1 $ dissect pt (p':ps)
+
+react' :: PolymerTracker -> Reactions -> PolymerTracker
+react' pt rs = M.foldrWithKey' f pt pt
+  where f :: Polymer -> Int -> PolymerTracker -> PolymerTracker
+        f (p:p':_) count acc | count > 0 = case M.lookup [p,p'] rs of
+                          Just i -> M.adjust (\c -> c - count) [p,p'] 
+                            $ M.insertWith (+) [i,p'] count 
+                            $ M.insertWith (+) [p,i] count acc
+                          Nothing -> acc
+        f _ _ acc = acc
+
+react :: Polymer -> Reactions -> Polymer
 react [] rs = []
 react (p:[]) rs = [p]
-react (p:p':ps) rs = case find ((== [p,p']) . fst) rs of
-  Just (_, i) -> p:i:react (p':ps) rs
+react (p:p':ps) rs = case M.lookup [p,p'] rs of
+  Just i -> p:i:react (p':ps) rs
   Nothing -> react (p':ps) rs
 
-reactTimes :: Int -> Polymer -> [Reaction] -> Polymer
+reactTimes :: Int -> Polymer -> Reactions -> Polymer
 reactTimes n p rs = times n (flip react rs) p
 
+reactTimes' :: Int -> Polymer -> Reactions -> PolymerTracker
+reactTimes' n p rs = times n (flip react' rs) (dissect M.empty p)
+
 score :: Polymer -> Int
-score p = sort p |> group |> fmap length|> \l -> maximum l - minimum l
+score p = sort p |> group |> fmap length |> \l -> maximum l - minimum l
+
+score' :: PolymerTracker -> Int
+score' p = M.toList p
+  |> fmap (\(p, c) -> (head p, c))
+  |> M.fromListWith (+)
+  |> M.toList
+  |> fmap snd
+  |> sort
+  |> \l -> maximum l - minimum l
 
 -- 2712
 solution1 :: Input -> Int
 solution1 input =
   parseOrDie inputParser input
+    |> fmap M.fromList
     |> uncurry (reactTimes 10)
     |> score
 
 solution2 :: Input -> Int
 solution2 input =
   parseOrDie inputParser input
-      |> uncurry (reactTimes 40)
-      |> score
+      |> fmap M.fromList
+      |> uncurry (reactTimes' 40)
+      |> score'
 
 verify :: IO Input -> Test
 verify input =
