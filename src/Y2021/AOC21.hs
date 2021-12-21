485ff0b06ec9a59ebcd5bd79d2a2ad7a1afc5738
Moritz Grauel <mgrauel@atlassian.com>
2021-12-21 20:56:11 +1100

    Day 21 cleaned

--- src/Y2021/AOC21.hs
+++ src/Y2021/AOC21.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Y2021.AOC21 where
 
-import Data.List (find, span)
+import Data.List (find)
+import qualified Data.Map.Strict as M
 import Data.Maybe (fromJust)
 import Test.HUnit (Test (TestCase, TestList), assertEqual)
+import Text.Parsec (newline, string)
 import Text.Parsec.ByteString (Parser)
-import Util (Input, parseOrDie, (|>))
-import Data.Bifunctor (bimap)
-import qualified Data.Map.Strict as M
+import Util (Input, number, parseOrDie, (|>))
+
+inputParser :: [Int] -> Parser GameState
+inputParser dice = do
+  p1 <- string "Player 1 starting position: " *> number <* newline
+  p2 <- string "Player 2 starting position: " *> number <* newline
+  pure $ mkGame p1 p2 dice
 
 data GameState = GameState
   { p1Pos :: Int,
@@ -28,11 +33,6 @@ instance Show GameState where
     show p1Points
       ++ "/"
       ++ show p2Points
---      ++ " ("
---      ++ show p1Pos
---      ++ ":"
---      ++ show p2Pos
---      ++ ")"
 
 data Player = P1 | P2 deriving (Eq, Show, Ord)
 
@@ -49,7 +49,7 @@ mkGame s1 s2 d =
     }
 
 updatePos :: Int -> Int -> Int
-updatePos m p =  cycle [1..10] !! (m + p - 1)
+updatePos m p = cycle [1 .. 10] !! (m + p - 1)
 
 gameStep :: GameState -> GameState
 gameStep g =
@@ -59,15 +59,10 @@ gameStep g =
         P1 -> g' {nextPlayer = P2, p1Pos = updatePos moves (p1Pos g), p1Points = p1Points g + updatePos moves (p1Pos g)}
         P2 -> g' {nextPlayer = P1, p2Pos = updatePos moves (p2Pos g), p2Points = p2Points g + updatePos moves (p2Pos g)}
 
-isWon :: GameState -> Bool
-isWon GameState {p1Points} | p1Points >= 1000 = True
-isWon GameState {p2Points} | p2Points >= 1000 = True
-isWon _ = False
-
-isEnd :: GameState -> Bool
-isEnd GameState {p1Points} | p1Points >= 21 = True
-isEnd GameState {p2Points} | p2Points >= 21 = True
-isEnd _ = False
+isWon :: Int -> GameState -> Bool
+isWon goal GameState {p1Points} | p1Points >= goal = True
+isWon goal GameState {p2Points} | p2Points >= goal = True
+isWon _ _ = False
 
 play :: GameState -> [GameState]
 play = iterate gameStep
@@ -76,97 +71,54 @@ score :: GameState -> Int
 score GameState {..} | p1Points > p2Points = p2Points * dieRolls
 score GameState {..} = p1Points * dieRolls
 
-inputParser :: Parser [String]
-inputParser = undefined
-
-playDirac :: [GameState] -> [GameState]
-playDirac gs = fmap diracStep gs
-  |> mconcat
-  |> \gs' -> if length gs' == length gs
-             then gs'
-             else playDirac gs'
-
 playDiracMap :: M.Map GameState Int -> M.Map GameState Int
 playDiracMap gmap = M.foldlWithKey f gmap gmap
-  where f :: M.Map GameState Int -> GameState -> Int -> M.Map GameState Int
-        f acc g _c | isEnd g = acc
-        f acc g c = diracStep g |> foldl (fi c) (M.delete g acc)
-        fi :: Int ->  M.Map GameState Int -> GameState -> M.Map GameState Int
-        fi gc acc g = M.insertWith (+) g gc acc
+  where
+    f :: M.Map GameState Int -> GameState -> Int -> M.Map GameState Int
+    f acc g _c | isWon 21 g = acc
+    f acc g c = diracStep g |> foldl (fi c) (M.delete g acc)
+    fi :: Int -> M.Map GameState Int -> GameState -> M.Map GameState Int
+    fi gc acc g = M.insertWith (+) g gc acc
 
 playDiracMapTilStable :: Int -> M.Map GameState Int -> M.Map GameState Int
 playDiracMapTilStable 0 _ = error "no stability reached"
 playDiracMapTilStable n g = let g' = playDiracMap g in if g == g' then g else playDiracMapTilStable (n - 1) g'
 
-splitP :: (a -> Bool) -> [a] -> ([a], [a]) -> ([a], [a])
-splitP _p [] r = r
-splitP p (a:as) (n, y) = if p a then splitP p as (n, a:y) else splitP p as (a:n, y)
-
-byWinningsM :: M.Map GameState Int -> (Int, Int)
-byWinningsM = M.foldlWithKey f (0,0)
-  where f :: (Int, Int) -> GameState -> Int -> (Int, Int)
-        f (p1, p2) g c = if p1Points g > p2Points g then (p1 + c,p2) else (p1, p2 + c)
-
-byWinnings :: [GameState] -> (Int, Int)
-byWinnings g = splitP (\g -> p1Points g > p2Points g) g ([],[])
- |> bimap length length
+byWinnings :: M.Map GameState Int -> (Int, Int)
+byWinnings = M.foldlWithKey f (0, 0)
+  where
+    f :: (Int, Int) -> GameState -> Int -> (Int, Int)
+    f (p1, p2) g c = if p1Points g > p2Points g then (p1 + c, p2) else (p1, p2 + c)
 
 diracStep :: GameState -> [GameState]
-diracStep g | isEnd g = [g]
-diracStep g =
-  [ gameStep (g {dice = [1, 1, 1]}),
-    gameStep (g {dice = [1, 1, 2]}),
-    gameStep (g {dice = [1, 1, 3]}),
-    gameStep (g {dice = [1, 2, 1]}),
-    gameStep (g {dice = [1, 2, 2]}),
-    gameStep (g {dice = [1, 2, 3]}),
-    gameStep (g {dice = [1, 3, 1]}),
-    gameStep (g {dice = [1, 3, 2]}),
-    gameStep (g {dice = [1, 3, 3]}),
-    gameStep (g {dice = [2, 1, 1]}),
-    gameStep (g {dice = [2, 1, 2]}),
-    gameStep (g {dice = [2, 1, 3]}),
-    gameStep (g {dice = [2, 2, 1]}),
-    gameStep (g {dice = [2, 2, 2]}),
-    gameStep (g {dice = [2, 2, 3]}),
-    gameStep (g {dice = [2, 3, 1]}),
-    gameStep (g {dice = [2, 3, 2]}),
-    gameStep (g {dice = [2, 3, 3]}),
-    gameStep (g {dice = [3, 1, 1]}),
-    gameStep (g {dice = [3, 1, 2]}),
-    gameStep (g {dice = [3, 1, 3]}),
-    gameStep (g {dice = [3, 2, 1]}),
-    gameStep (g {dice = [3, 2, 2]}),
-    gameStep (g {dice = [3, 2, 3]}),
-    gameStep (g {dice = [3, 3, 1]}),
-    gameStep (g {dice = [3, 3, 2]}),
-    gameStep (g {dice = [3, 3, 3]})
-  ]
-
--- mkGame 4 8 (cycle [1,2,3])
+diracStep g | isWon 21 g = [g]
+diracStep g = do
+  d1 <- [1 .. 3]
+  d2 <- [1 .. 3]
+  d3 <- [1 .. 3]
+  pure $ gameStep (g {dice = [d1, d2, d3]})
 
 -- 855624
 solution1 :: Input -> Int
-solution1 _input = mkGame 4 10 (cycle [1 .. 100]) |> play |> find isWon |> fmap score |> fromJust
+solution1 input =
+  parseOrDie (inputParser (cycle [1 .. 100])) input
+    |> play
+    |> find (isWon 1000)
+    |> fmap score
+    |> fromJust
 
--- parseOrDie inputParser input
---  |> error "not yet"
-
-maxPair :: (Int, Int) -> Int
-maxPair (x,y) | x> y = x
-maxPair (_,y) = y
-
+-- 187451244607486
 solution2 :: Input -> Int
-solution2 _input = mkGame 4 10 [] |> (`M.singleton` 1) |> playDiracMapTilStable 20 |> byWinningsM |> maxPair
-  --parseOrDie inputParser input
-    -- |> error "not yet"
+solution2 input =
+  parseOrDie (inputParser []) input
+    |> (`M.singleton` 1)
+    |> playDiracMapTilStable 20
+    |> byWinnings
+    |> uncurry max
 
 verify :: IO Input -> Test
 verify input =
   TestList
     [ TestCase $ assertEqual "solution 1" 855624 . solution1 =<< input,
-      TestCase $ assertEqual "solution 2" undefined . solution2 =<< input
+      TestCase $ assertEqual "solution 2" 187451244607486 . solution2 =<< input
     ]
-
-testData :: Input
-testData = ""
