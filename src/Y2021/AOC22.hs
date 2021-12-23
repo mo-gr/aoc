57ae57e5c1c5741b93cde54be5ece9c1adbecdba
Moritz Grauel <mgrauel@atlassian.com>
2021-12-23 12:15:41 +1100

    Day 22

--- src/Y2021/AOC22.hs
+++ src/Y2021/AOC22.hs
@@ -2,66 +2,99 @@
 
 module Y2021.AOC22 where
 
-import Test.HUnit (Test (TestCase, TestList), assertEqual)
-import Text.Parsec.ByteString (Parser)
-import Util (Input, parseOrDie, (|>), negativeNumber)
 import Data.Functor (($>))
-import Text.Parsec (string, (<|>), newline, many1, try)
-import qualified Data.Set as S
+import Data.Maybe (catMaybes)
+import Test.HUnit (Test (TestCase, TestList), assertEqual)
+import Text.Parsec (many1, newline, string, try, (<|>))
+import Text.Parsec.ByteString (Parser)
+import Util (Input, negativeNumber, parseOrDie, (|>))
+import Data.Foldable (foldr')
 
 type Point = (Int, Int, Int)
 
-type Range = ([Int], [Int], [Int])
+data Cube = Cube
+  { enabled :: Bool,
+    start :: Point,
+    end :: Point
+  }
+  deriving (Eq, Show, Ord)
 
-data Step = Enable Range | Disable Range deriving (Eq, Show)
+cubeParser :: Parser Cube
+cubeParser = do
+  cubeState <- try (string "on " $> True) <|> (string "off " $> False)
+  xCubeStart <- string "x=" *> negativeNumber
+  xCubeEnd <- string ".." *> negativeNumber <* string ","
+  yCubeStart <- string "y=" *> negativeNumber
+  yCubeEnd <- string ".." *> negativeNumber <* string ","
+  zCubeStart <- string "z=" *> negativeNumber
+  zCubeEnd <- string ".." *> negativeNumber <* newline
+  pure $ Cube cubeState (xCubeStart, yCubeStart, zCubeStart) (xCubeEnd, yCubeEnd, zCubeEnd)
 
-stepParser :: Parser Step
-stepParser = do
-  stepType <- try (string "on " $> Enable) <|> (string "off " $> Disable)
-  xRangeStart <- string "x=" *> negativeNumber
-  xRangeEnd <- string ".." *> negativeNumber <* string ","
-  yRangeStart <- string "y=" *> negativeNumber
-  yRangeEnd <- string ".." *> negativeNumber <* string ","
-  zRangeStart <- string "z=" *> negativeNumber
-  zRangeEnd <- string ".." *> negativeNumber <* newline
-  --pure $ stepType ([(max xRangeStart (-50)) .. (min xRangeEnd 50)], [(max yRangeStart (-50)) .. (min yRangeEnd 50)], [(max zRangeStart (-50)) .. (min zRangeEnd 50)])
-  pure $ stepType ([xRangeStart .. xRangeEnd], [yRangeStart .. yRangeEnd], [zRangeStart .. zRangeEnd])
+inputParser :: Parser [Cube]
+inputParser = many1 cubeParser
 
-inputParser :: Parser [Step]
-inputParser = many1 stepParser
+pairwise :: (Int -> Int -> Int) -> Point -> Point -> Point
+pairwise f (a, b, c) (a', b', c') = (f a a', f b b', f c c')
 
-cubesInRange :: Range -> S.Set Point
-cubesInRange (xs, ys, zs) = S.fromList $ do
-  z <- zs
-  y <- ys
-  x <- xs
-  pure (x,y,z)
---  if (x < -50) || (x > 50) || (y < -50) || (y > 50) || (z < -50) || (z > 50)
---  then [] else pure (x,y,z)
+volume :: Cube -> Int
+volume c = pairwise dist (start c) (end c) |> pointProduct
+  where
+    dist x x' = 1 + abs (x' - x)
+    pointProduct (x, y, z) = x * y * z
 
-runProcedure :: Step -> S.Set Point -> S.Set Point
-runProcedure (Enable r) enabled = S.union enabled $ cubesInRange r
-runProcedure (Disable r) enabled = S.difference enabled (cubesInRange r)
+noOverlap :: Cube -> Cube -> Bool
+noOverlap c c' = isBefore (end c) (start c') || isBefore (end c') (start c)
+  where
+    isBefore :: Point -> Point -> Bool
+    isBefore (x, y, z) (x', y', z') = x < x' || y < y' || z < z'
 
+intersectionCube :: Cube -> Cube -> Maybe Cube
+intersectionCube c c' = if noOverlap c c' then Nothing else
+  Just $
+    Cube
+      { enabled = not $ enabled c',
+        start = pairwise max (start c) (start c'),
+        end = pairwise min (end c) (end c')
+      }
+
+overlapVolume :: Cube -> [Cube] -> Int
+overlapVolume c cs = sum $ f <$> zip [1..] cs
+  where f (i,cc) = case intersectionCube c cc of
+                      Nothing -> 0
+                      Just ic -> volume ic - overlapVolume ic (drop i cs)
+
+runReverse :: [Cube] -> Int
+runReverse cs = snd $ foldr' process ([], 0) cs
+  where process :: Cube -> ([Cube], Int) -> ([Cube], Int)
+        process c (acc, vol)| not $ enabled c = (c:acc, vol)
+        process c (acc, vol) = (c:acc, vol + volume c - overlapVolume c acc)
+
+countEnabled :: [Cube] -> Int
+countEnabled [] = 0
+countEnabled (c : cs) | enabled c = volume c + countEnabled cs
+countEnabled (c : cs) = negate (volume c) + countEnabled cs
+
+cropTo50 :: Cube -> Maybe Cube
+cropTo50 c = intersectionCube (Cube (not $ enabled c) (-50, -50, -50) (50, 50, 50)) (c {enabled = not $ enabled c}) 
 
 -- 588120
 solution1 :: Input -> Int
 solution1 input =
   parseOrDie inputParser input
-    |> foldl (flip runProcedure) S.empty
-    |> length
+    |> fmap cropTo50
+    |> catMaybes
+    |> runReverse
 
 solution2 :: Input -> Int
 solution2 input =
   parseOrDie inputParser input
-    |> error "not yet"
+    |> runReverse
 
+-- 1134088247046731
 verify :: IO Input -> Test
 verify input =
   TestList
     [ TestCase $ assertEqual "solution 1" 588120 . solution1 =<< input,
-      TestCase $ assertEqual "solution 2" undefined . solution2 =<< input
+      TestCase $ assertEqual "solution 2" 1134088247046731 . solution2 =<< input
     ]
 
-testData :: Input
-testData = "on x=10..12,y=10..12,z=10..12\non x=11..13,y=11..13,z=11..13\noff x=9..11,y=9..11,z=9..11\non x=10..10,y=10..10,z=10..10\noff x=79458..91210,y=-6760..4282,z=1518..13350\n"
