3bbb187fc28540cf7d303ecf080f5c1766014869
Moritz Grauel <mgrauel@atlassian.com>
2022-01-11 11:31:08 +1100

    2015 D17

--- src/Y2015/AOC.hs
+++ src/Y2015/AOC.hs
@@ -9,6 +9,7 @@ import qualified Y2015.AOC13
 import qualified Y2015.AOC14
 import qualified Y2015.AOC15
 import qualified Y2015.AOC16
+import qualified Y2015.AOC17
 import qualified Y2015.AOC2
 import qualified Y2015.AOC3
 import qualified Y2015.AOC4
@@ -35,6 +36,7 @@ lookUpDay D13 = Y2015.AOC13.solution
 lookUpDay D14 = Y2015.AOC14.solution
 lookUpDay D15 = Y2015.AOC15.solution
 lookUpDay D16 = Y2015.AOC16.solution
+lookUpDay D17 = Y2015.AOC17.solution
 lookUpDay _ = error "not yet"
 
 year :: Year
---
+++ src/Y2015/AOC17.hs
@@ -0,0 +1,39 @@
+module Y2015.AOC17 where
+
+import AOC (Solution (PureSolution))
+import Control.Applicative
+import Control.Monad.Logic
+import Data.List (subsequences)
+import Util (Input, (|>))
+
+choose :: [a] -> Logic a
+choose = foldr ((<|>) . pure) empty
+
+waysToFill :: Int -> [Int] -> [[Int]]
+waysToFill capacity containers = observeAll $ do
+  selection <- choose $ subsequences containers
+  guard $ sum selection == capacity
+  pure selection
+
+filterMinLength :: [[Int]] -> [[Int]]
+filterMinLength xs = let minSize = minimum $ length <$> xs in filter ((== minSize) . length) xs
+
+-- 1304
+solution1 :: Input -> Int
+solution1 _input =
+  waysToFill 150 availableContainers
+    |> length
+
+-- 18
+solution2 :: Input -> Int
+solution2 _input =
+  waysToFill 150 availableContainers
+    |> filterMinLength
+    |> length
+
+testData, availableContainers :: [Int]
+testData = [20, 15, 10, 5, 5]
+availableContainers = [33, 14, 18, 20, 45, 35, 16, 35, 1, 13, 18, 13, 50, 44, 48, 6, 24, 41, 30, 42]
+
+solution :: Solution
+solution = PureSolution solution1 1304 solution2 18
