061a50aa839fff74bd65ca91798ba9a81725de0e
Moritz Grauel <mgrauel@atlassian.com>
2022-03-14 15:59:42 +1100

    2016 21

--- src/Y2016/AOC21.hs
+++ src/Y2016/AOC21.hs
@@ -3,14 +3,35 @@
 module Y2016.AOC21 (solution) where
 
 import AOC (Solution (PureSolution))
+import Control.Applicative ((<|>))
+import Control.Monad (msum)
 import Data.List (elemIndex)
-import Text.Parsec (many1, newline, string, letter, try)
+import Text.Parsec (letter, many1, newline, string, try)
 import Text.Parsec.ByteString (Parser)
 import Util (Input, number, parseOrDie, (|>))
-import Control.Monad (msum)
-import Control.Applicative ((<|>))
 
 type Operation = String -> String
+
+data Op = SwapPos Int Int | Rotate Int | SwapLetter Char Char | RotatePos Char | MovePos Int Int | ReversePos Int Int
+
+applyOp :: Op -> Operation
+applyOp (ReversePos n m) = reversePos n m
+applyOp (MovePos n m) = movePos n m
+applyOp (RotatePos n) = rotatePos n
+applyOp (SwapLetter n m) = swapLetter n m
+applyOp (Rotate n) = rotate n
+applyOp (SwapPos n m) = swapPos n m
+
+reverseOp :: Op -> Operation
+reverseOp (MovePos n m) = movePos m n
+reverseOp (Rotate n) = rotate (negate n)
+reverseOp (SwapLetter n m) = swapLetter n m
+reverseOp (ReversePos n m) = reversePos n m
+reverseOp (SwapPos n m) = swapPos n m
+reverseOp (RotatePos n) = bruteReverseRotate n
+
+bruteReverseRotate :: Char -> Operation
+bruteReverseRotate c str = head . filter (\b -> str == rotatePos c b) $ flip rotate str <$> [0 ..]
 
 swapPos :: Int -> Int -> Operation
 swapPos x y inp =
@@ -62,59 +83,45 @@ movePos x y inp = recur 0 inp
       | idx == y = head rst : inp !! x : recur (succ idx) (tail rst)
       | otherwise = head rst : recur (succ idx) (tail rst)
 
-inputParser :: Parser [Operation]
+inputParser :: Parser [Op]
 inputParser = many1 operation
   where
     operation = msum [try rotRightP, try rotLeftP, try swapP, try reverseP, try moveP, try swapLetterP, rotPosP] <* newline
-    rotRightP = rotate <$> (string "rotate right " *> number <* (try (string " steps") <|> string " step"))
-    rotLeftP = rotate . negate <$> (string "rotate left " *> number <* (try (string " steps") <|> string " step"))
+    rotRightP = Rotate <$> (string "rotate right " *> number <* (try (string " steps") <|> string " step"))
+    rotLeftP = Rotate . negate <$> (string "rotate left " *> number <* (try (string " steps") <|> string " step"))
     swapP = do
       _ <- string "swap position "
       p <- number
       _ <- string " with position "
-      swapPos p <$> number
+      SwapPos p <$> number
     reverseP = do
       _ <- string "reverse positions "
       p <- number
       _ <- string " through "
-      reversePos p <$> number
+      ReversePos p <$> number
     moveP = do
       _ <- string "move position "
       p <- number
-      _ <-string " to position "
-      movePos p <$> number
+      _ <- string " to position "
+      MovePos p <$> number
     swapLetterP = do
       _ <- string "swap letter "
       p <- letter
       _ <- string " with letter "
-      swapLetter p <$> letter
-    rotPosP = rotatePos <$> (string "rotate based on position of letter " *> letter)
+      SwapLetter p <$> letter
+    rotPosP = RotatePos <$> (string "rotate based on position of letter " *> letter)
 
 -- gcedfahb
 solution1 :: Input -> String
 solution1 input =
   parseOrDie inputParser input
-    |> foldl (\acc op -> op acc) "abcdefgh" 
+    |> foldl (flip applyOp) "abcdefgh"
 
+-- hegbdcfa
 solution2 :: Input -> String
 solution2 input =
   parseOrDie inputParser input
-    |> error "not yet"
-
-testData :: Input
-testData = "reverse positions 2 through 5\nrotate based on position of letter d\nreverse positions 1 through 7\nrotate right 1 step\n"
-
-example :: Operation
-example inp =
-  inp
-    |> swapPos 4 0
-    |> swapLetter 'd' 'b'
-    |> reversePos 0 4
-    |> rotate (-1)
-    |> movePos 1 4
-    |> movePos 3 0
-    |> rotatePos 'b'
-    |> rotatePos 'd'
+    |> foldr reverseOp "fbgdceah"
 
 solution :: Solution
-solution = PureSolution solution1 "gcedfahb" solution2 undefined
+solution = PureSolution solution1 "gcedfahb" solution2 "hegbdcfa"
