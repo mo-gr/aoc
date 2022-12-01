8e8fe511dc4146fb774e4a186a0b3be37cf022fc
Moritz Grauel <mgrauel@atlassian.com>
2022-12-01 20:25:52 +1100

    2022 1

--- src/Y2022/AOC1.hs
+++ src/Y2022/AOC1.hs
@@ -4,20 +4,32 @@ module Y2022.AOC1 where
 
 import AOC (Solution (PureSolution))
 import Text.Parsec.ByteString (Parser)
-import Util (Input, parseOrDie, (|>))
+import Util (Input, parseOrDie, (|>), number)
+import Text.Parsec (many1, newline)
+import Data.List (sort)
 
-inputParser :: Parser Int
-inputParser = undefined
+inputParser :: Parser [[Int]]
+inputParser = many1 (elfP <* newline)
+  where elfP :: Parser [Int]
+        elfP = many1 (number <* newline)
 
+-- 69281
 solution1 :: Input -> Int
 solution1 input =
   parseOrDie inputParser input
-    |> undefined
+    |> fmap sum
+    |> sort
+    |> last
 
+-- 201524
 solution2 :: Input -> Int
 solution2 input =
   parseOrDie inputParser input
-    |> undefined
+    |> fmap sum
+    |> sort
+    |> reverse
+    |> take 3
+    |> sum
 
 solution :: Solution
-solution = PureSolution solution1 undefined solution2 undefined
+solution = PureSolution solution1 69281 solution2 201524
