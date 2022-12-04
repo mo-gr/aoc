2ce91227488eb0e57048984346c896543c9abbd0
Moritz Grauel <mgrauel@atlassian.com>
2022-12-04 08:38:32 +0100

    2022 3

--- src/Y2022/AOC3.hs
+++ src/Y2022/AOC3.hs
@@ -3,21 +3,56 @@
 module Y2022.AOC3 where
 
 import AOC (Solution (PureSolution))
+import Data.Char (isUpper)
+import qualified Data.Set as S
+import Text.Parsec (letter, many1, newline)
 import Text.Parsec.ByteString (Parser)
 import Util (Input, parseOrDie, (|>))
 
-inputParser :: Parser Int
-inputParser = undefined
+inputParser :: Parser [String]
+inputParser = many1 (many1 letter <* newline)
 
+splitSet :: String -> (S.Set Char, S.Set Char)
+splitSet s = (S.fromList (take half s), S.fromList (drop half s))
+  where
+    half = length s `div` 2
+
+common :: (S.Set Char, S.Set Char) -> S.Set Char
+common (s, s') = S.intersection s s'
+
+points :: Char -> Int
+points c | isUpper c = 27 + fromEnum c - fromEnum 'A'
+points c = 1 + fromEnum c - fromEnum 'a'
+
+-- 7850
 solution1 :: Input -> Int
 solution1 input =
   parseOrDie inputParser input
-    |> undefined
+    |> fmap splitSet
+    |> fmap common
+    |> fmap (head . S.toList)
+    |> fmap points
+    |> sum
 
+threes :: [a] -> [(a, a, a)]
+threes [] = []
+threes (a : b : c : rest) = (a, b, c) : threes rest
+threes _ = error "no threes grouping"
+
+badge :: (String, String, String) -> Char
+badge (a, b, c) =
+  S.intersection (S.intersection (S.fromList a) (S.fromList b)) (S.fromList c)
+    |> S.toList
+    |> head
+
+-- 2581
 solution2 :: Input -> Int
 solution2 input =
   parseOrDie inputParser input
-    |> undefined
+    |> threes
+    |> fmap badge
+    |> fmap points
+    |> sum
 
 solution :: Solution
-solution = PureSolution solution1 undefined solution2 undefined
+solution = PureSolution solution1 7850 solution2 2581
