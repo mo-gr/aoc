2ee405667bdbcb0dcef9bfc97d2079d60db9120d
Moritz Grauel <mgrauel@atlassian.com>
2022-12-22 16:00:35 +0100

    2022 20

--- src/Y2022/AOC20.hs
+++ src/Y2022/AOC20.hs
@@ -3,7 +3,7 @@
 module Y2022.AOC20 where
 
 import AOC (Solution (PureSolution))
-import Data.List (delete, elemIndex, foldl')
+import Data.List (delete, elemIndex, find, foldl')
 import Text.Parsec (many, newline)
 import Text.Parsec.ByteString (Parser)
 import Util (Input, negativeNumber, parseOrDie, (|>))
@@ -11,22 +11,18 @@ import Util (Input, negativeNumber, parseOrDie, (|>))
 inputParser :: Parser [Int]
 inputParser = many (negativeNumber <* newline)
 
-mix :: [Int] -> [Int]
-mix xsOriginal = foldl' mixit xsOriginal xsOriginal
-
-mixit :: [Int] -> Int -> [Int]
-mixit xs x =
-  case elemIndex x xs of
-    Nothing -> error "elem not found"
-    Just idx ->
-      let idx' = mod (idx + x) (pred $ length xs)
-       in ( if idx == idx'
-              then xs
-              else take idx' (delete x xs) <> [x] <> drop idx' (delete x xs)
-          )
-            |> cycle
-            |> drop idx
-            |> take (length xs)
+mixit :: [(Int, Int)] -> Int -> [(Int, Int)]
+mixit xs xx =
+  let x = case snd <$> find ((== xx) . fst) xs of
+        Just i -> i
+        _ -> error "index not found"
+   in case elemIndex (xx, x) xs of
+        Nothing -> error "elem not found"
+        Just idx ->
+          let idx' = mod (idx + x) (pred $ length xs)
+           in if idx == idx'
+                then xs
+                else take idx' (delete (xx, x) xs) <> [(xx, x)] <> drop idx' (delete (xx, x) xs)
 
 solve1 :: [Int] -> Int
 solve1 xs =
@@ -34,11 +30,10 @@ solve1 xs =
     + (mixed !! ((zeroIdx + 2000) `mod` length mixed))
     + (mixed !! ((zeroIdx + 3000) `mod` length mixed))
   where
-    mixed = mix xs
+    mixed = foldl' mixit (zip [0 ..] xs) [0 .. pred (length xs)] |> fmap snd
     zeroIdx = case elemIndex 0 mixed of
       Just idx -> idx
       Nothing -> error "no 0 found"
-
 
 -- 1591
 solution1 :: Input -> Int
@@ -59,34 +54,25 @@ solve2 xs =
       Just idx -> idx
       Nothing -> error "no 0 found"
     keyed = (* key) <$> xs
-    mixed = foldl' mixit keyed keyed
-          |> flip (foldl' mixit) keyed
-          |> flip (foldl' mixit) keyed
-          |> flip (foldl' mixit) keyed
-          |> flip (foldl' mixit) keyed
-          |> flip (foldl' mixit) keyed
-          |> flip (foldl' mixit) keyed
-          |> flip (foldl' mixit) keyed
-          |> flip (foldl' mixit) keyed
-          |> flip (foldl' mixit) keyed
+    idxes = [0 .. pred (length xs)]
+    mixed =
+      foldl' mixit (zip [0 ..] keyed) idxes
+        |> flip (foldl' mixit) idxes
+        |> flip (foldl' mixit) idxes
+        |> flip (foldl' mixit) idxes
+        |> flip (foldl' mixit) idxes
+        |> flip (foldl' mixit) idxes
+        |> flip (foldl' mixit) idxes
+        |> flip (foldl' mixit) idxes
+        |> flip (foldl' mixit) idxes
+        |> flip (foldl' mixit) idxes
+        |> fmap snd
 
-
--- too low 9843764836737
--- too low 8416991105763
+-- 14579387544492
 solution2 :: Input -> Int
 solution2 input =
   parseOrDie inputParser input
     |> solve2
 
 solution :: Solution
-solution = PureSolution solution1 1591 solution2 undefined
-
-testData :: Input
-testData =
-  "1\n\
-  \2\n\
-  \-3\n\
-  \3\n\
-  \-2\n\
-  \0\n\
-  \4\n"
+solution = PureSolution solution1 1591 solution2 14579387544492
